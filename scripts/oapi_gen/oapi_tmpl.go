6203f1b9370cf31c6dd39239e6421c6a1901c401
Jim Kalafut <jkalafut@hashicorp.com>
2018-04-05 08:37:58 -0700

    Update description

--- scripts/oapi_gen/oapi_tmpl.go
+++ scripts/oapi_gen/oapi_tmpl.go
@@ -3,10 +3,12 @@ package main
 const tmpl = `swagger: "2.0"
 info:
   title: HashiCorp Vault API
+  description: |
+    The Vault HTTP API gives you full access to Vault via HTTP. Every aspect of Vault can be controlled via this API. The Vault CLI uses the HTTP API to access Vault. You can read more about the Vault API at: https://www.vaultproject.io/api/index.html
   version: {{ .Version }}
   contact:
     name: HashiCorp Vault
-    url: https://www.vaultproject.io/api/index.html
+    url: https://www.vaultproject.io
   license:
     name: Mozilla Public License 2.0
     url: https://www.mozilla.org/en-US/MPL/2.0
